920295be05a5915b13bd44685d1f4924693897ea
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-01-19 16:41:52 +0100

    Creato model Dress + chiamata db nel file .env

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dress;
 
 class HomeController extends Controller
 {
-    //
+    public function index() {
+        $all_dresses = Dress::all();
+        $data = [
+            'dresses' => $all_dresses
+        ];
+        return view('home', $data);
+    }
 }
